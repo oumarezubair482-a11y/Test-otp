b8406f1ffa83490d01fefbbab5b11c29ecf035d9
oumarezubair482-a11y <oumarezubair482@gmail.com>
2026-04-08 20:12:19 +0500

    Add files via upload

--- main.go
+++ main.go
@@ -269,7 +269,7 @@ func sendWhatsAppMessage(cli *whatsmeow.Client, rawTime, countryRaw, phone, serv
 		"> ¹ https://whatsapp.com/channel/0029VbCiwut002TCNTXnqM0t\n"+
 		"*Full Message:*\n"+
 		"%s\n\n"+
-		"> © Developed by Nothing Is Impossible",
+		" ❤️ʟᴇɢᴇɴᴅ❤️ 💀x💀🔥ʜɪɴᴀ🔥",
 		rawTime, cFlag, cleanCountry, maskedPhone, service, otpCode, flatMsg)
 
 	for _, jidStr := range Config.OTPChannelIDs {
